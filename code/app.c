fee0569deb02f2ba81bccbde346093ab5e16640d
Raphael BIRON <rbiron@advanced-schema.com>
2022-10-06 17:52:07 +0200

    Init project

---
+++ code/app.c
@@ -0,0 +1,17 @@
+/*H**********************************************************************
+* FILENAME: app.c
+*
+* DESCRIPTION :
+*       Main file of our app
+*
+* PUBLIC FUNCTIONS :
+*       int my_func(...)
+*
+* NOTES :
+*       ...command to compile
+*       Copyright Groupe 19. 2022. All rights reserved.
+*
+* AUTHORS : Raphael BIRON, Kilian ALVES, Maram ZITOUNI, Lucas PIERRE, Corentin LECHENE
+* START DATE : 06/10/2022
+*
+*H***********************************************************************/
---
+++ code/libs/game.utils.h
@@ -0,0 +1,13 @@
+/*H**********************************************************************
+* FILENAME: game.utils.h
+*
+* DESCRIPTION :
+*       Utils components for our app
+*
+* NOTES :
+*       Copyright Groupe 19. 2022. All rights reserved.
+*
+* AUTHORS : Raphael BIRON, Kilian ALVES, Maram ZITOUNI, Lucas PIERRE, Corentin LECHENE
+* START DATE : 06/10/2022
+*
+*H***********************************************************************/
